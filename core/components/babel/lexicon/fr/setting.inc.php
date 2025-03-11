df4f33642c34f13d40678d93ba70b2470d030e79
EpopE <github@intersel.org>
2025-03-11 21:26:03 +0100

    Added translation using Weblate (French)

---
+++ core/components/babel/lexicon/fr/setting.inc.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Setting Lexicon Entries for Babel
+ *
+ * @package babel
+ * @subpackage lexicon
+ */
+$_lang['setting_babel.babelTvName'] = '';
+$_lang['setting_babel.babelTvName_desc'] = '';
+$_lang['setting_babel.contextKeys'] = '';
+$_lang['setting_babel.contextKeys_desc'] = '';
+$_lang['setting_babel.debug'] = '';
+$_lang['setting_babel.debug_desc'] = '';
+$_lang['setting_babel.displayText'] = '';
+$_lang['setting_babel.displayText_desc'] = '';
+$_lang['setting_babel.restrictToGroup'] = '';
+$_lang['setting_babel.restrictToGroup_desc'] = '';
+$_lang['setting_babel.syncFields'] = '';
+$_lang['setting_babel.syncFields_desc'] = '';
+$_lang['setting_babel.syncTvs'] = '';
+$_lang['setting_babel.syncTvs_desc'] = '';
