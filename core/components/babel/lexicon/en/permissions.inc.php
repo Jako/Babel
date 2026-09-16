e39d99aa1b79b3bc5b30266b4ccd118c9f05c8e2
Thomas Jakobi <thomas.jakobi@partout.info>
2026-09-17 01:21:29 +0200

    Update lexicon

--- core/components/babel/lexicon/en/permissions.inc.php
+++ core/components/babel/lexicon/en/permissions.inc.php
@@ -5,4 +5,4 @@
  * @package babel
  * @subpackage lexicon
  */
-$_lang['babel.permission.babel_settings_desc'] = 'To edit the Babel setting in the Babel custom manager page.';
+$_lang['babel.permission.babel_settings_desc'] = 'To edit the Babel settings in the Babel custom manager page.';
